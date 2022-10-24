a8ead5c1cb94306983a52bf364c8e41ceb018ff2
Hillel1408 <etemax@bk.ru>
2022-10-25 00:25:06 +0300

    task-1

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,5 +1,5 @@
 const { src, dest } = require('gulp');
-const postcss = require('gulp-postcss')
+const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync');
 const sourcemaps = require('gulp-sourcemaps');
@@ -12,27 +12,26 @@ const csso = require('postcss-csso');
 const { paths, isProduction } = require('../config');
 
 const commonPlugins = [
-  autoprefixer(),
-  postcssNormalize(),
-]
-
-const plugins = [
-  ...commonPlugins,
-  csso()
+    autoprefixer({
+        grid: true,
+        overrideBrowserslist: ['last 5 versions'],
+        cascade: true,
+    }),
+    postcssNormalize(),
 ];
 
+const plugins = [...commonPlugins, csso()];
+
 const styles = () => {
-  return (
-    src(paths.styles.src)
-      .pipe(plumber())
-      .pipe(gulpIf(!isProduction, sourcemaps.init()))
-      .pipe(sass().on('error', sass.logError))
-      .pipe(postcss(isProduction ? plugins : commonPlugins))
-      .pipe(gulpIf(!isProduction, sourcemaps.write('.')))
-      .pipe(plumber.stop())
-      .pipe(dest(paths.styles.dist))
-      .pipe(browserSync.stream())
-  )
-}
+    return src(paths.styles.src)
+        .pipe(plumber())
+        .pipe(gulpIf(!isProduction, sourcemaps.init()))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(isProduction ? plugins : commonPlugins))
+        .pipe(gulpIf(!isProduction, sourcemaps.write('.')))
+        .pipe(plumber.stop())
+        .pipe(dest(paths.styles.dist))
+        .pipe(browserSync.stream());
+};
 
 module.exports = styles;
--- src/components/sections/consultation/consultation.js
+++ src/components/sections/consultation/consultation.js
@@ -3,15 +3,7 @@ let inputsForm = document.querySelectorAll('.consultation__form-input');
 
 btnForm.addEventListener('click', function (e) {
     e.preventDefault();
-    let data = [];
-    inputsForm.forEach((item) => {
-        let obj = {
-            name: '',
-            value: '',
-        };
-        obj.name = item.name;
-        obj.value = item.value;
-        data.push(obj);
-    });
-    console.log(data); //отправляем данные на сервер (у нас в консоль)
+    let obj = {};
+    inputsForm.forEach((item) => (obj[item.name] = item.value));
+    console.log(obj);
 });
